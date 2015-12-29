79b5190ee5379552c5866770803368d4840f20e0
Ulises Cabrera <ucabrera@outlook.com>
2015-12-29 04:24:42 +0000

    styled single poll section

--- public/js/controllers/singlePollController.js
+++ public/js/controllers/singlePollController.js
@@ -1,21 +1,32 @@
-angular.module('SinglePollModule', ['UsersService', 'PollsService'])
+/*global randomColor, angular */
+
+angular.module('SinglePollModule', ['UsersService', 'PollsService', 'chart.js'])
     .controller('SinglePollController',['$scope','UsersSvc','$routeParams','PollsSvc', function($scope, UsersSvc, $routeParams, PollsSvc) {
     // will display error message here        
     $scope.errorPollMessage = '';
     
-    var data = {
-      // A labels array that can contain any sort of values
-      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-      // Our series array that contains series objects or in this case series data arrays
-      series: [
-        [5, 2, 4, 2, 0]
-      ]
+    // will hold labels and votes counts of the data coming from the server
+    $scope.labels = [];
+    $scope.data = [];
+    
+    // returns 40 random colors to use on the chart
+    $scope.colors = randomColor({ count: 40, luminosity: 'dark'});
+    
+    // function to push data to chart
+    var pushDataToChart = function(choices){
+        choices.forEach(function(choice){
+            $scope.labels.push(choice.choiceName);
+            $scope.data.push(choice.votes);
+        });
     };
     
-    // Create a new line chart object where as first parameter we pass in a selector
-    // that is resolving to our chart container element. The Second parameter
-    // is the actual data object.
-    new Chartist.Line('.ct-chart', data);      
+    // function to clear the chart before updating it with the new vote
+    var resetChart = function(){
+        $scope.labels =[];
+        $scope.data = [];
+    };
+        
+        
     
     // will hold title params to use on the url
     var id = $routeParams.id; 
@@ -24,8 +35,8 @@ angular.module('SinglePollModule', ['UsersService', 'PollsService'])
         .then(
             function(res){
             	if (res.data.state === 'success') {
+            	    pushDataToChart(res.data.poll.choices);
             		$scope.poll = res.data.poll;
-            		$scope.hello= "hello from single poll and here is the title " + $scope.poll.title;
             	} else {
             		$scope.errorPollMessage = res.data.message;
             	}
@@ -42,6 +53,8 @@ angular.module('SinglePollModule', ['UsersService', 'PollsService'])
                     if(res.data.state === 'success') {
                         console.log('Vote successfully');
                         // render the updated poll from the sucessful vote action
+                        resetChart();
+                        pushDataToChart(res.data.poll.choices);
                         $scope.poll = res.data.poll;
                     } else {
                         $scope.errorPollMessage = res.data.message;
